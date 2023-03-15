1a534c755bc9eac6974047001a9fceadb2f8467a
Hector DelaRosa <hdelaros@gmail.com>
2023-03-15 12:11:07 -0400

    Refactored code to return boolens instead of printing to console

--- quiz_logic.py
+++ quiz_logic.py
@@ -16,22 +16,19 @@ class QuizLogic:
 
     def next_question(self):
         index = self.question_number
+        self.question_number += 1
         self.current_question = self.question_list[index]
         current_question_text = html.unescape(self.current_question.text)
-        return f'Q.{self.question_number + 1} {current_question_text}'
+        return f'Q.{self.question_number} {current_question_text}'
         # self.check_answer(response, current_question.answer)
 
     def check_answer(self, user_response):
         correct_answer = self.current_question.answer
         if user_response.lower() == correct_answer.lower():
-            print('You got the correct answer!')
             self.score += 1
+            return True
         else:
-            print('You got the wrong answer.')
-        self.question_number += 1
-
-        print(f'The correct answer was {correct_answer} ')
-        print(f'Your score is {self.score}/{self.question_number}\n')
+            return False
 
     def end_of_quiz(self):
         print('You completed the quiz!')
