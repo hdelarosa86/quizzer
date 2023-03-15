276f05a4295683f852f5898d4db7dfe3ef663d1c
Hector DelaRosa <hdelaros@gmail.com>
2023-03-15 11:35:24 -0400

    Refactored code and removed while loop

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ question_bank = [Question(question['question'], question['correct_answer']) for 
 
 quiz_logic = QuizLogic(question_bank)
 
-# while quiz_logic.still_have_questions():
-#     quiz_logic.next_question()
-
-quizzer = QuizInterface()
+quizzer = QuizInterface(quiz_logic)
 
 quiz_logic.end_of_quiz()
