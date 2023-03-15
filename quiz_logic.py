f56dad11efdbb56786e9cde66d5f3a742bd4e27b
Hector DelaRosa <hdelaros@gmail.com>
2023-03-15 11:34:52 -0400

    Refactored code and set current_question as a property

--- quiz_logic.py
+++ quiz_logic.py
@@ -5,6 +5,7 @@ class QuizLogic:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.current_question = {}
         self.score = 0
 
     def still_have_questions(self):
@@ -15,21 +16,21 @@ class QuizLogic:
 
     def next_question(self):
         index = self.question_number
-        current_question = self.question_list[index]
-        current_question_text = html.unescape(current_question.text)
-        response = input(f'Q.{self.question_number + 1} {current_question_text} True/False: ')
-        self.check_answer(response, current_question.answer)
-        return response
+        self.current_question = self.question_list[index]
+        current_question_text = html.unescape(self.current_question.text)
+        return f'Q.{self.question_number + 1} {current_question_text}'
+        # self.check_answer(response, current_question.answer)
 
-    def check_answer(self, user_response, answer):
-        if user_response.lower() == answer.lower():
+    def check_answer(self, user_response):
+        correct_answer = self.current_question.answer
+        if user_response.lower() == correct_answer.lower():
             print('You got the correct answer!')
             self.score += 1
         else:
             print('You got the wrong answer.')
         self.question_number += 1
 
-        print(f'The correct answer was {answer} ')
+        print(f'The correct answer was {correct_answer} ')
         print(f'Your score is {self.score}/{self.question_number}\n')
 
     def end_of_quiz(self):
