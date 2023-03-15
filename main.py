8b3d16847f1578dc0996482dc6a73571ddc1709f
Hector DelaRosa <hdelaros@gmail.com>
2023-03-15 10:16:00 -0400

    Created QuizInterface class with Tkinter and added it to main.py; commented out while loop

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from data import question_data
 from question_model import Question
 from quiz_logic import QuizLogic
+from ui import QuizInterface
 
 question_bank = [Question(question['question'], question['correct_answer']) for question in question_data]
 
 quiz_logic = QuizLogic(question_bank)
 
-while quiz_logic.still_have_questions():
-    quiz_logic.next_question()
+# while quiz_logic.still_have_questions():
+#     quiz_logic.next_question()
+
+quizzer = QuizInterface()
 
 quiz_logic.end_of_quiz()
