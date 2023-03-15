b86be58d39efe6904c6758ddbf817061e8876d22
Hector DelaRosa <hdelaros@gmail.com>
2023-03-15 09:57:41 -0400

    refactored code in data.py, added game logic to main.py

--- data.py
+++ data.py
@@ -9,4 +9,4 @@ response = requests.get('https://opentdb.com/api.php', params=params)
 response.raise_for_status()
 data = response.json()
 
-questions = data['results']
+question_data = data['results']
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-from data import questions
+from data import question_data
+from question_model import Question
+from quiz_logic import QuizLogic
 
+question_bank = [Question(question['question'], question['correct_answer']) for question in question_data]
+
+quiz_logic = QuizLogic(question_bank)
+
+while quiz_logic.still_have_questions():
+    quiz_logic.next_question()
+
+quiz_logic.end_of_quiz()
